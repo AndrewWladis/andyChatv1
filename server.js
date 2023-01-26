a329635959d9216afec46e2fd95e0b7cbbe9306b
AndrewWladis <andrewwladis@gmail.com>
2023-01-26 13:04:49 -0500

    added bad word filter

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const io = require('socket.io')(3000)
 const users = {}
+var Filter = require('bad-words');
+var customFilter = new Filter({ placeHolder: '💀'});
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
@@ -7,7 +9,7 @@ io.on('connection', socket => {
     socket.broadcast.emit('user-connected', name)
   })
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    socket.broadcast.emit('chat-message', { message: customFilter.clean(message), name: users[socket.id] })
   })
   socket.on('send-andymoji', moji => {
     socket.broadcast.emit('andymoji-message', { andimoji: moji, name: users[socket.id] })
