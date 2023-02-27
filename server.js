1890f4fde45c32eb84821ab455ce8117e020d18c
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-02-27 12:34:46 -0500

    went to good version

--- server.js
+++ server.js
@@ -2,28 +2,20 @@ const io = require('socket.io')(3000)
 const users = {}
 var Filter = require('bad-words');
 var customFilter = new Filter({ placeHolder: '💀'});
-let i = 0;
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
-    if ([undefined, null, "null"].indexOf(name) != -1) {
-      users[socket.id] = name
-      socket.broadcast.emit('user-connected', name)
-    }
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: customFilter.clean(message), name: users[socket.id] })
-  })
-  socket.on('send-breaking-bad-quote', message => {
-    socket.broadcast.emit('chat-message', { message: customFilter.clean(message.quote), name: message.author })
   })
   socket.on('send-andymoji', moji => {
     socket.broadcast.emit('andymoji-message', { andimoji: moji, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
-    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
-      socket.broadcast.emit('user-disconnected', users[socket.id])
-    }
+    socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
 })
