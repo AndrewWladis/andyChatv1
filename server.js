22fe5a29cf8e07dd974b3cb5dff12866e5670403
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-02-27 12:07:12 -0500

    test

--- server.js
+++ server.js
@@ -12,19 +12,13 @@ io.on('connection', socket => {
     }
   })
   socket.on('send-chat-message', message => {
-    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
-      socket.broadcast.emit('chat-message', { message: customFilter.clean(message), name: users[socket.id] })
-    }
+    socket.broadcast.emit('chat-message', { message: customFilter.clean(message), name: users[socket.id] })
   })
   socket.on('send-breaking-bad-quote', message => {
-    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
-      socket.broadcast.emit('chat-message', { message: customFilter.clean(message.quote), name: message.author })
-    }
+    socket.broadcast.emit('chat-message', { message: customFilter.clean(message.quote), name: message.author })
   })
   socket.on('send-andymoji', moji => {
-    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
-      socket.broadcast.emit('andymoji-message', { andimoji: moji, name: users[socket.id] })
-    }
+    socket.broadcast.emit('andymoji-message', { andimoji: moji, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
     if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
