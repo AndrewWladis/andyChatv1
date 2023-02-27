13b5e12d46c5d8bc1dede1e5d38c79c5a67292aa
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-02-27 11:53:18 -0500

    added tests

--- server.js
+++ server.js
@@ -2,31 +2,33 @@ const io = require('socket.io')(3000)
 const users = {}
 var Filter = require('bad-words');
 var customFilter = new Filter({ placeHolder: '💀'});
+let i = 0;
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
-    if ([undefined, null, ""].indexOf(name) != -1) {
+    console.log(name, i, typeof(name))
+    if ([undefined, null, "null"].indexOf(name) != -1) {
       users[socket.id] = name
       socket.broadcast.emit('user-connected', name)
     }
   })
   socket.on('send-chat-message', message => {
-    if ([undefined, null, ""].indexOf(users[socket.id]) != -1) {
+    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
       socket.broadcast.emit('chat-message', { message: customFilter.clean(message), name: users[socket.id] })
     }
   })
   socket.on('send-breaking-bad-quote', message => {
-    if ([undefined, null, ""].indexOf(users[socket.id]) != -1) {
+    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
       socket.broadcast.emit('chat-message', { message: customFilter.clean(message.quote), name: message.author })
     }
   })
   socket.on('send-andymoji', moji => {
-    if ([undefined, null, ""].indexOf(users[socket.id]) != -1) {
+    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
       socket.broadcast.emit('andymoji-message', { andimoji: moji, name: users[socket.id] })
     }
   })
   socket.on('disconnect', () => {
-    if ([undefined, null, ""].indexOf(users[socket.id]) != -1) {
+    if ([undefined, null, "null"].indexOf(users[socket.id]) != -1) {
       socket.broadcast.emit('user-disconnected', users[socket.id])
     }
     delete users[socket.id]
