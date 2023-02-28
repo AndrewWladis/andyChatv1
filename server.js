2c32553ec677b158ad3b82d1f26964747272cea5
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-02-28 10:39:59 -0500

    tried new way to get around cors error

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const io = require('socket.io')(3000);
-io.set('origins', '*:*');
+const io = require("socket.io")(3000, {
+  handlePreflightRequest: (req, res) => {
+      const headers = {
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+  }
+});
+
 const users = {}
 var Filter = require('bad-words');
 var customFilter = new Filter({ placeHolder: '💀'});
